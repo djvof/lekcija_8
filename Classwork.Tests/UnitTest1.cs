c0b75ab7b03e3e173af1818585e9d6efd656ed84
Vitaly <vit@bb-tech.eu>
2021-10-20 20:19:50 +0300

    classwork

---
+++ Classwork.Tests/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Xunit;
+
+namespace Classwork.Tests
+{
+    ///  <summary>
+    /// Ja skaitlis dalās ar 3 tad atgriez "Fizz"
+    /// Ja skaitlis dalās ar 5 tad atgriez "Buzz"
+    /// Ja skaitlis dalās ar 3 un 5 tad atgriez "FizzBuzz"
+    /// Ja skaitlis nedalās ne ar 3 un ne ar 5 tad atgriez skaitli
+    /// </summary>
+
+    public class FizzBuzzTests
+    {
+        // Function_When_Then
+        [Fact]
+        public void GetNumber_WhenNumberDoesNotDivideBy3Or5_ThenReturnsNumber()
+        {
+            //Arrange
+            var fizzClass = new FizzBuzz();
+
+            //Act
+            var fizzText = fizzClass.GetNumber(1);
+
+            // Assert
+            Assert.Equal("1", fizzText);
+
+
+        }
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        [InlineData(21)]
+        [InlineData(12)]
+        public void GetNumber_WhenNumberDividesBy3_ThenReturnsFizz(int testNumber)
+        {
+            //Arrange
+            var fizzClass = new FizzBuzz();
+
+            //Act
+            var fizzText = fizzClass.GetNumber(testNumber);
+
+            // Assert
+            Assert.Equal("Fizz", fizzText);
+
+
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(25)]
+        [InlineData(20)]
+        public void GetNumber_WhenNumberDividesBy5_ThenReturnsBuzz(int testableNumber)
+        {
+            // Arrange
+            var fizzClass = new FizzBuzz();
+
+            // Act
+            var fizzText = fizzClass.GetNumber(testableNumber);
+
+            // Assert
+            Assert.Equal("Buzz", fizzText);
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        [InlineData(60)]
+        [InlineData(90)]
+        public void GetNumber_WhenNumberDividesBy3And5_ThenReturnsFizzBuzz(int testableNumber)
+        {
+            // Arrange
+            var fizzClass = new FizzBuzz();
+
+            // Act
+            var fizzText = fizzClass.GetNumber(testableNumber);
+
+            // Assert
+            Assert.Equal("FizzBuzz", fizzText);
+        }
+
+    }
+}
---
+++ Classwork/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork
+{
+    public class FizzBuzz
+    {
+        public string GetNumber(int number)
+        {
+
+            string text = "";
+
+            if (number % 3 == 0)
+            {
+                text += "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                text += "Buzz";
+            }
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return number.ToString();
+            }
+
+            return text;
+        }
+
+    }
+}
